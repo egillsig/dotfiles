1846640086168efbc112a138b74ca200c486edaa
egill <egill@reontech.com>
2016-07-14 09:43:23 +0000

    fasd installation ready

--- setup.sh
+++ setup.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
 
-ROOT_UID=0      # Only users with $UID 0 have root privileges.
-E_NOTROOT=87    # Non-root exit error.
-OH_MY_ZSH_URL="https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
-USERNAME='egill'
-PACKAGE_MANAGER='/usr/bin/apt'
+USERNAME="egill"
+HOME_DIR="/home/$USERNAME"
+DOTFILES_DIR="$HOME_DIR/dotfiles"
+OH_MY_ZSH_DIR="$HOME_DIR/.oh-my-zsh"
+FASD_DIR="$HOME_DIR/fasd"
+PACKAGE_MANAGER="/usr/bin/apt"
 
-if [ "$UID" -ne "$ROOT_UID" ]
-then
-    echo "Must be root to run this script"
-    exit $E_NOTROOT
+sudo $PACKAGE_MANAGER install zsh vim tree curl git wget
+sudo chsh -s `which zsh` $USERNAME
+
+# Install oh-my-zsh (source: github.com/robbyrussell/oh-my-zsh/blob/master/tools/install.sh)
+if [ -d "$OH_MY_ZSH_DIR" ]; then
+    printf "Oh My Zsh already installed.\n"
+else
+    # Prevent the cloned repository from having insecure permissions. Failing to do
+    # so causes compinit() calls to fail with "command not found: compdef" errors
+    # for users with insecure umasks (e.g., "002", allowing group writability).
+    umask g-w,o-w
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $OH_MY_ZSH_DIR || {
+        printf "Error: git clone of oh-my-zsh repo failed\n"
+    }
 fi
 
-$PACKAGE_MANAGER install zsh vim tree curl git wget ipython
-chsh -s `which zsh` $USERNAME
-
-su - $USERNAME
-wget -qO install_ohmyzsh.sh $OH_MY_ZSH_URL
-. install_ohmyzsh.sh
-rm install_ohmyzsh.sh
-
-# retrieve dotfiles
-
+# Replace dotfiles
 # Vim automatically installs vim-plug and plugins when opened
-ln -s ~/.vim/vimrc ~/.vimrc
-# vim +PluginInstall +qall
+if [ -f $HOME_DIR/.vimrc ]; then
+    rm -v $HOME_DIR/.vimrc
+fi
+ln -vs $DOTFILES_DIR/vimrc $HOME_DIR/.vimrc
 
-# Install fasd
-add-apt-repository ppa:aacebedo/fasd
-apt update
-apt install fasd
+if [ -f $HOME_DIR/.zshrc ]; then
+    rm -v $HOME_DIR/.zshrc
+fi
+ln -vs $DOTFILES_DIR/zshrc $HOME_DIR/.zshrc
+
+# Install fasd with apt on ubuntu
+if grep -qi 'derpderp' /proc/version;
+then
+    echo "Installing fasd via apt";
+    if ! dpkg -s fasd > /dev/null 2>&1;
+    then
+        sudo add-apt-repository ppa:aacebedo/fasd
+        sudo apt update
+        sudo apt install fasd
+    else
+        echo "fasd already installed"
+    fi;
+else
+    echo "Installing fasd from source";
+    git clone --depth=1 https://github.com/clvv/fasd.git $FASD_DIR || {
+        echo "Error: git clone of fasd repo failed"
+    }
+    cd $FASD_DIR
+    sudo make install
+fi;
+
